18c804a48362d8a5854a12961ce8c742000067b0
Lucas Monteiro de Sousa <107234259+LMsousa20@users.noreply.github.com>
2022-09-13 01:14:51 -0300

    ajuste finais e bootstrap

--- scritp.js
+++ scritp.js
@@ -2,10 +2,17 @@ let palavraChave = '';
 let palavraDigitada = ['a'];
 let pontuacao = 0;
 let buscaPalavras =['âmago','negro','êxito','mexer','termo','nobre','senso','algoz','afeto','plena','ética','mútua','tênue','sutil','vigor','aquém','fazer','porém','audaz','assim','seção','sanar','inato','cerne','fosse','ideia','poder','moral','desde','muito','torpe','justo','honra','fútil','sobre','quiçá','anexo','razão','etnia','ícone','sonho','tange','égide','amigo','lapso','mútuo','expor','haver','hábil','tempo','dengo','seara','então','pesar','ávido','ardil','genro','posse','boçal','coser','causa','dizer','corja','prole','pária','detém','dever','tenaz','graça','brado','saber','óbice','crivo','digno','ápice','ânimo','ânsia','comum','ceder','temor','sendo','culto','gleba','assaz','atroz','pauta','mundo','censo','fugaz','ainda','valha','cozer','denso','estar','vício','neném','forte','vulgo','revés','pudor','regra','dogma','louco','criar','banal','saúde','round','impor','jeito','atrás','ordem','clava','tenro','pífio','desse','apraz','pedir','mercê','reaça','usura','feliz','prosa','servo','juízo','sábio','viril','coisa','presa','manso','ontem','mesmo','falar','cunho','forma','devir','meiga','afago','fluir','ébrio','limbo','platô','vendo','sério','posso','guisa','mágoa','herói','visar','temer','acaso','puder','pleno','valor','cisma','lugar','certo','bruma','ímpio','afins','óbvio','êxodo','gerar','obter','crise','matiz','praxe','todos','senil','havia','fluxo','vênia','abrir','enfim','garbo','tédio','FALSO','ritmo','álibi','união','tomar','fácil','visão','legal','parvo','pulha','valia','reter','vital','bravo','favor','grato','burro','prumo','vivaz','gênio','olhar','parco','laico','levar','possa','reles','ameno','casta','tecer','óbito','morte','prime','selar','ranço','noção','fator','anelo','sábia','noite','façam','falta','rogar','farsa','achar','cabal','citar','nicho','adiar','coeso','brega','épico','cisão','ouvir','viver','fardo','ativo','força','linda','sinto','gente','passo','exato','sonso','imune','dúbio','tendo','único','amplo','leigo','calma','cesta','sesta','sulco','lavra','rever','revel','ajuda','deter','vemos','labor','humor','pobre','carma','gesto','árduo','outro','atuar','feixe','ciúme','tende','ótica','velho','claro','igual','ponto','hiato','toada','ideal','sonsa','débil','vácuo','terno','ambos','remir','fusão','marco','senão','fonte','terra','probo','jovem','leito','capaz','inata','advém','doido','horda','xeque','ficar','relva','tenra','líder','algum','farão','velar','série','caçar','apoio','vazio','coçar','tanto','anuir','vimos','entre','rigor','verso','pouco','botar','frase','dorso','sente','signo','coesa','raiva','cruel','massa','papel','moção','prece','feito','ambas','fauna','minha','morar','vírus','torço','peste','casto','credo','covil','preso','ciclo','ímpar','lazer','brisa','faina','furor','dócil','chata','flora','blasé','maior','vetor','trama','árido','nossa','pegar','houve','adeus','aceso','beata','manha','banzo','setor','vulto','liame','senda','seita','breve','livro','chuva','vasto','ardor','salvo','peixe','visse','sorte','morro','pecha','comer','plano','carro','antro','prado','átomo','reger','saiba','ocaso','segue','rezar','avaro','nunca','pajem','aliás','saída','ótimo','áureo','junto','mudar','chulo','acima','anciã','serão','opção','fitar','parar','fruir','jazia','treta','lenda','fugir','nação','grupo','andar','leite','brava','bando','prazo','gerir','norma','motim','rapaz','campo','alude','tosco','parte','época','tenso','sinhá','ídolo','exame','arcar','avião','psico','tirar','reino','praia','agora','vilão','malta','venal','risco','praga','soldo','índio','quota','aonde','antes','corpo','logro','sumir','preto','traga','voraz','cheio','quase','fixar','virão','pompa','filho','texto','certa','exijo','turba','cópia','prova','apego','nódoa','oásis','estão','alado','caixa','solto','turva','acesa','conta','verbo','ligar','messe','nível','átrio','grave','doído','tocar','verve','festa','apelo','áurea','parva','lindo','trupe','fraco','pardo','livre','oxalá','dessa','manhã','afora','fenda','sabia','opaco','parca','alçar','fatos','viria','navio','jirau','mente','elite','astro','fatal','faixa','ficha','retém','lidar','tinha','ético','magia','glosa','firme','autor','supra','grata','verba','bater','reses','cioso','pique','calda','salve','cousa','junco','deixa','privê','irmão','douto','pagão','atual','macio','molho','vezes','longe','sexta','posto','abriu','light','supor','torso','sarça','extra','turvo','porta','curso','judeu','cânon','asilo','ígneo','caber','órfão','besta','locus','drops','rouca','rádio','desta','bioma','nosso','vídeo','combo','vosso','zelar','ruína','júlia','vinha','culpa','menos','bônus','ereto','agudo','finda','paira','advir','feudo','facto','urgia','traço','cútis','super','sítio','meigo','surja','facho','tetra','suave','amena','autos','rumor','tento','pilar','turma','cocho','chama','clean','louça','museu','ações','lápis','mosto','lasso','local','geral','acolá','hobby','pódio','optar','boato','medir','rubro','refém','brabo','pacto','aluno','páreo','crime','folga','drama','ponha','poema','penta','mesma','letal','chato','móvel','cacho','metiê','feroz','ávida','vigia','aroma','hoste','monte','teste','golpe','piada','pasmo','vetar','cover','açude','rival','fazia','coral','troça','ecoar','finjo','poeta','axila','fórum','lição','daqui','verde','passa','forem','clima','riste','carta','monge','plebe','cetro','artur','tacha','súcia','lesse','ateia','calmo','ébano','escol','busca','falha','briga','venha','roupa','idoso','plumo','conto','macro','tribo','sarau','aviso','plaga','cargo','tarde','perco','fruto','viram','chefe','farta','átimo','légua','vento','grama','berro','civil','virar','amiga','catre','arado','única','corso','ornar','seixo','surto','manga','saldo','pedra','assar','traje','nuvem','bruta','beijo','fosso','troca','bença','mangá','casar','recém','deste','magna','gíria','úteis','sósia','trato','tiver','porte','itens','vedar','tição','arfar','depor','tutor','amado','silvo','canso','irado','bazar','âmbar','deram','vazão','grota','pinho','gabar','cifra','renda','perto','rural','jejum','bruto','mídia','laudo','inter','pavor','feita','pasma','régio','tchau','nesse','clero','fossa','segar','órgão','amada','odiar','minar','troco','vadio','vagar','molde','pomar','stand','sótão','mamãe','aviar','negar','rocha','lesto','cenho','canto','cinto','lesão','close','visto','bolsa','largo','morfo','paiol','xucro','horto','densa','proto','ruído','marca','invés','jogar','ileso','pugna','vista','chula','podar','urdir','penso','ufano','vasta','mocho','cheia','frota','cível','úmido','bulir','piche','úbere','linha','volta','esgar','santo','tenha','resto','peita','apear','ágape','fundo','velha','logos','misto','cerca','manto','nessa','natal','narco','ferpa','monta','jazer','verão','demão','pólis','símio','findo','campa','preço','barão','fazes','chaga','ardis','etapa','nesta','letra','coroa','álbum','mover','cosmo','trago','álamo','seiva','matar','banto','retro','troço','final','venho','folia','ceita','áudio','punha','redor','barro','sabor','louro','bolso','dança','calça','rente','folha','queda','sigla','todas','arroz','firma','salva','farol','salmo','burra','coevo','lutar','lousa','mimar','torna','neste','enjoo','solta','míope','baixa','tumba','fugiu','veloz','ousar','calor','corar','zumbi','calvo','nácar','justa','longo','reler','gueto','forro','vazia','fátuo','bedel','chave','penca','obtém','logia','cacto','vário','farto','lucro','mania','sexto','quite','sugar','repor','subir','custo','puído','urgir','staff','passe','ultra','sadio','valer','refil','viger','versa','hífen','louca','nariz','cardo','lento','usual','sócio','rédea','choça','sofia','corte','modal','ceifa','ferir','mouro','aéreo','garra','dados','voilà','outra','nesga','anzol','abade','pluma','diabo','pátio','grife','sabiá','raiar','lábil','trair','pavio','skate','xampu','pagar','pazes','roubo','dique','disso','podre','tacho','filme','surra','bruxa','tíbio','jeová','digna','dobro','abono','couve','cisco','farei','caibo','posta','notar','domar','quais','falsa','régia','barra','volto','nisso' ];
+let CERTA =[];
+let QUASE = [];
+let ERRADA = [];
+let tentativas = 0;
+
+
 
 function buscando() {
     let codigo = Math.random();
     palavraChave = (buscaPalavras[(codigo * 1000).toFixed(0)]).toUpperCase();
+    document.getElementById('erros').style.display= 'none';
     
 }
 
@@ -19,31 +26,55 @@ function testar() {
 
     console.log(palavraDigitada);
 
-    comparar()
-    console.log(`pontuacao`,pontuacao)
+    comparar();
+    console.log(`pontuacao`,pontuacao);
 }
 
 function comparar() {
     pontuacao = 0;
+    tentativas++;
     for(let i=0; i<palavraChave.length; i++){
+
         if(palavraChave[i] === palavraDigitada[i]){
+
             document.getElementById(`ltr${i}`).style.backgroundColor = 'green';
             document.getElementById(`ltr${i}`).style.color = 'white';
             pontuacao ++;
-            document.getElementById('letrasStatus').innerHTML += `<span class="CERTA"> ${palavraDigitada[i]} </span>`
+
+            if(CERTA.indexOf(palavraDigitada[i]) === -1){
+
+                document.getElementById('CERTA').innerHTML += `${palavraDigitada[i]}`
+            }
+            CERTA.push(palavraDigitada[i])
+
 
         }else{
+            
             document.getElementById(`ltr${i}`).style.backgroundColor = 'red';
             document.getElementById(`ltr${i}`).style.color = 'white';
-            document.getElementById('letrasStatus').innerHTML += `<span class="ERRADA"> ${palavraDigitada[i]} </span>`
+         
+
+            
             for(let v=0; v<palavraDigitada.length; v++){
-                if(palavraChave[v] === palavraDigitada[i]){
-                    document.getElementById(`ltr${i}`).style.backgroundColor = 'yellow';
+                if(palavraChave.indexOf(palavraDigitada[i]) !== -1){
+                          document.getElementById(`ltr${i}`).style.backgroundColor = 'yellow';
                     document.getElementById(`ltr${i}`).style.color = 'black';
-                    document.getElementById('letrasStatus').innerHTML += `<span class="QUASE"> ${palavraDigitada[i]} </span>`
+                    
+                    if(QUASE.indexOf(palavraDigitada[i]) === -1){
+                        document.getElementById('QUASE').innerHTML += `${palavraDigitada[i]}`
+                    }
+                    QUASE.push(palavraDigitada[i])
+                    
                     console.log(i,v)
+                    console.log(QUASE.indexOf(palavraDigitada[i]))
+
                 }
             }
+
+            if(ERRADA.indexOf(palavraDigitada[i]) === -1 && QUASE.indexOf(palavraDigitada[i]) <= -1){
+                document.getElementById('ERRADA').innerHTML += `${palavraDigitada[i]}`
+            }
+            ERRADA.push(palavraDigitada[i])
         
         }
 
@@ -55,8 +86,7 @@ function comparar() {
      
        }else{
         document.getElementById('resultado').style.backgroundColor = 'RED';
-        document.getElementById('resultado').innerHTML += `${palavraDigitada.join('')} está incorreta <BR>`;
-        
+        adicionarErro()
      
        }
     }
@@ -67,3 +97,17 @@ function comparar() {
         }
         location.reload();
     }
+
+function adicionarErro(){
+    if(tentativas != 0){
+        document.getElementById('erros').style.display= '';
+    
+        document.getElementById('tituloErros').innerHTML = `<span>ERROS</span>`;
+    }
+
+    document.getElementById('resultado').innerHTML += `${palavraDigitada.join('')} <BR>`;
+    if(tentativas > 5){
+        alert('Ja usou as 5 tentativas')
+        reiniciar()
+    }
+            }
